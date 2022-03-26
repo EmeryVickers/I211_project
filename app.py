26a6af41f18a05942969b7442f9eab50e613f1c5
Emery Vickers <emrvick@iu.edu>
2022-03-26 13:31:08 -0400

    index is working!

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
